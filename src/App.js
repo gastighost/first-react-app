c8f6f13518f699d010dea78ff786cc4a4616b739
Gaston Roxas <gacaro51597@gmail.com>
2022-03-23 16:56:43 +0100

    added rating component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import Products from './Products'
+import Products from './Products';
+import Rating from './Rating';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div>
-      <h1>
-        Learn React Hooks
-        <Products />
-      </h1>
+      <Rating rating="1"/>
+      <Rating rating="2"/>
+      <Rating rating="3"/>
+      <Rating rating="4"/>
+      <Rating rating="5"/>
     </div>
   );
 }
